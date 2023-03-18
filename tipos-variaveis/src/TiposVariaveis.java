b987e14182898356befa5655670f9d0fcb0fb39d
luizfernando03 <luizfernandodemoura.2013@gmail.com>
2023-03-18 18:54:47 -0300

    Subindo classes

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,15 @@
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+    }
+}
